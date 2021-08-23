4fbc4d520d08457720fc9ebaf2838c1efdf34917
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-23 07:35:25 -0500

    refactor(app_menu): responsive

--- lib/ui/shared/app_menu.dart
+++ lib/ui/shared/app_menu.dart
@@ -8,10 +8,63 @@ class AppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    return LayoutBuilder(
+      builder: ( _ , constraints) => 
+        (constraints.maxWidth > 520)
+        ? _TabletDesktopMenu()
+        : _MobileMenu()
+    );
+  }
+}
+
+class _TabletDesktopMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
       width: double.infinity,
       child: Row(
+        children: [
+          CustomFlatButton(
+            text: 'Contador Stateful',
+            // onPressed: () => Navigator.pushNamed(context, '/stateful'),
+            onPressed: () => locator<NavigationService>().navigateTo('/stateful'),
+            color: Colors.black,
+          ),
+
+          SizedBox(width: 10),
+
+          CustomFlatButton(
+            text: 'Contador Provider',
+            onPressed: () => locator<NavigationService>().navigateTo('/provider'),
+            color: Colors.black,
+          ),
+
+          SizedBox(width: 10),
+
+          CustomFlatButton(
+            text: 'Otra página',
+            onPressed: () => locator<NavigationService>().navigateTo('/abc'),
+            color: Colors.black,
+          )
+        ],
+      )
+    );
+  }
+}
+
+
+class _MobileMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
